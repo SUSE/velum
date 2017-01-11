ea525c2292893abe603f9f1953f215cb20b5909a
Rafael Fernández López <rfernandezlopez@suse.com>
2017-01-11 16:18:10 +0100

    Fix devise usage, use email for login and remove name from schema and migration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@
 # User represents administrators in this application.
 class User < ApplicationRecord
   enabled_devise_modules = [:database_authenticatable, :registerable,
-                            :recoverable, :rememberable, :trackable, :validatable,
-                            authentication_keys: [:name]]
+                            :recoverable, :rememberable, :trackable, :validatable].freeze
 
   devise(*enabled_devise_modules)
 
-  validates :name, :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
