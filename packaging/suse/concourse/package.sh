6807ee73a1593fbc349ead74c31f64908480f2d2
Maximilian Meister <mmeister@suse.de>
2017-02-18 18:31:58 +0100

    packaging: strip resource from resource name
    in the UI it is already visible that it is a resource, so no reason
    to explicitly tag it

--- packaging/suse/concourse/package.sh
+++ packaging/suse/concourse/package.sh
@@ -9,17 +9,17 @@ sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
 # build the spec file
 log "building the spec file"
-velum-git-resource/packaging/suse/make_spec.sh velum
+velum-git/packaging/suse/make_spec.sh velum
 
-pushd velum-osc-resource/Virtualization:containers:Velum/velum 1> /dev/null
+pushd velum-osc/Virtualization:containers:Velum/velum 1> /dev/null
   log "updating osc checkout to newest revision"
   osc up
   log "removing old specfile"
   osc rm velum.spec
   log "adding new specfile"
-  cp ../../../velum-git-resource/packaging/suse/velum.spec .
+  cp ../../../velum-git/packaging/suse/velum.spec .
   log "fetching newest release from github"
-  wget --no-check-certificate -O master.tar.gz https://github.com/kubic-project/velum/archive/master.tar.gz
+  wget -O master.tar.gz https://github.com/kubic-project/velum/archive/master.tar.gz
 popd 1> /dev/null
 
-cp -a velum-osc-resource/Virtualization:containers:Velum/velum/. velum-osc-updated-resource/
+cp -a velum-osc/Virtualization:containers:Velum/velum/. velum-osc-updated/
