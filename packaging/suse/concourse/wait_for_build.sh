e1b4a9619b8506e1431f3364a5e7ec391cc4c391
Maximilian Meister <mmeister@suse.de>
2017-02-22 15:03:23 +0100

    packaging: add project/package/apiurl parameter
    this makes scripts reusable for usage with different projects (ibs)
    
    also the project name in obs has changed
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- packaging/suse/concourse/wait_for_build.sh
+++ packaging/suse/concourse/wait_for_build.sh
@@ -2,11 +2,27 @@
 
 set -e
 
+if [ -z "$2" ]; then
+  cat <<EOF
+usage:
+  ./wait_for_build.sh PROJECT PACKAGE [APIURL]
+EOF
+  exit 1
+fi
+
 sed -i "s|<username>|$OSC_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
+project=$1
+package=$2
+if [ -z "$3" ]; then
+  apiurl=https://api.opensuse.org
+else
+  apiurl=$3
+fi
+
 log() { echo ">>> $1" ; }
-get_result() { osc results Virtualization:containers:Velum velum ; }
+get_result() { osc -A $apiurl results $project $package ; }
 
 until get_result | grep -q "building"
 do
