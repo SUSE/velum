a39c74d4346c1702ce6dd1f1ba72f8456e27cff7
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-03-06 15:02:14 +0200

    Add the Logout link again to make tests pass (styling pending)

--- spec/features/auth/logout_feature_spec.rb
+++ spec/features/auth/logout_feature_spec.rb
@@ -9,12 +9,12 @@ feature "Logout feature" do
   end
 
   scenario "Redirects to login screen" do
-    click_link("logout")
+    click_link("Logout")
     expect(current_url).to eq root_url
   end
 
   scenario "After login guest redirects to login page when he attempts to access dashboard again" do
-    click_link("logout")
+    click_link("Logout")
     visit root_url
     expect(page).to have_content("Log in")
   end
