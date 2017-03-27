3058b3d41bef58321211366f5facb11e9195f17d
Maximilian Meister <mmeister@suse.de>
2017-03-27 13:13:57 +0200

    setup: enable select field for the country codes
    like that we can drop the front-end validation, and it's more convenient
    for the user
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- app/helpers/setup_helper.rb
+++ app/helpers/setup_helper.rb
@@ -4,8 +4,8 @@ require "yaml"
 # SetupHelper contains all the setup view helpers.
 module SetupHelper
   # https://www.iso.org/iso-3166-country-codes.html
-  # taken from https://coderwall.com/p/xww5mq/two-letter-country-code-regex
-  def country_code_regex
-    YAML.load_file(Rails.root.join("config", "country_codes.yml"))["iso"][3166]["regex"]
+  def country_codes
+    YAML.load_file(Rails.root.join("config", "country_codes.yml"))["iso"][3166]["codes"]
+        .to_a.map(&:reverse)
   end
 end
