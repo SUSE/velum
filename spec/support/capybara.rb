a38c8aabccc54a727f12d363085a54a8027d5fb3
Vítor Avelino <contact@vitoravelino.me>
2017-05-31 16:34:38 -0300

    spec: added newer phantomjs version from gem

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,6 +7,7 @@ require "capybara/poltergeist"
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
     app,
+    phantomjs:         Phantomjs.path,
     js_errors:         false,
     phantomjs_options: ["--load-images=no"]
   )
