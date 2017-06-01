e3b744e6ec73646922a1138a5f9abe388363ed00
Vítor Avelino <contact@vitoravelino.me>
2017-06-01 17:06:51 -0300

    dashboard: updated layout

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,26 @@
 MinionPoller = {
   poll: function() {
     this.request();
-    return setInterval(this.request, 5000);
+  },
+
+  stateWeight: function(state) {
+    // I guess updates should be 2
+
+    switch (state) {
+      case 'failed':
+        return 3;
+      case 'pending':
+        return 1;
+      // applied, not_applied
+      default:
+        return 0;
+    }
+  },
+
+  sortMinions: function(minions) {
+    minions.sort(function(a, b) {
+      return MinionPoller.stateWeight(b.highstate)- MinionPoller.stateWeight(a.highstate);
+    });
   },
 
   request: function() {
@@ -24,6 +43,8 @@ MinionPoller = {
         var unassignedMinions = data.unassigned_minions || [];
         if (MinionPoller.renderMode == "discovery") {
           minions = minions.concat(unassignedMinions);
+        } else {
+          MinionPoller.sortMinions(minions);
         }
 
         for (i = 0; i < minions.length; i++) {
@@ -39,25 +60,32 @@ MinionPoller = {
         $(".nodes-container tbody").html(rendered);
 
         // disable bootstrap button if there are no minions
-        if (minions.length == 0) {
-          $("#bootstrap").prop('disabled', true);
-          MinionPoller.enable_kubeconfig(false);
-        } else {
-          $("#bootstrap").prop('disabled', false);
-          MinionPoller.enable_kubeconfig(allApplied);
-        }
+        $("#bootstrap").prop('disabled', minions.length === 0);
+
+        MinionPoller.enable_kubeconfig(minions.length > 0 && allApplied);
+
+        $('.assigned-count').text(minions.length);
+        $('.master-count').text(MinionPoller.selectedMasters.length);
 
         if (unassignedMinions.length > 0) {
           if ($("#node-count").length > 0) {
             $("#node-count").text(unassignedMinions.length + " nodes found");
           } else {
-            $('#unassigned_count').html(unassignedMinions.length + " \
-            <strong>new</strong> nodes are available but have not been added to the cluster yet");
+            $('.unassigned-count').text(unassignedMinions.length);
           }
         } else {
-          $('#unassigned_count').html('');
+          $('.unassigned-count').text(0);
         }
+
+        // remove loading and shows content
+        $('.summary-loading').hide();
+        $('.summary-content').removeClass('hidden');
+        $('.nodes-loading').hide();
+        $('.nodes-content').removeClass('hidden');
       }
+    }).always(function() {
+      // make another request only after the last one finished
+      setTimeout(MinionPoller.request, 5000);
     });
   },
 
@@ -72,20 +100,20 @@ MinionPoller = {
 
     switch(minion.highstate) {
       case "not_applied":
-        statusHtml = '<i class="fa fa-circle-o text-success fa-2x" aria-hidden="true"></i>';
+        statusHtml = '<i class="fa fa-circle-o text-success fa-2x" aria-hidden="true" title="Not applied"></i>';
         break;
       case "pending":
         statusHtml = '\
-          <span class="fa-stack" aria-hidden="true">\
+          <span class="fa-stack" aria-hidden="true" title="Applying">\
             <i class="fa fa-circle fa-stack-2x text-success" aria-hidden="true"></i>\
             <i class="fa fa-refresh fa-stack-1x fa-spin fa-inverse" aria-hidden="true"></i>\
           </span>';
         break;
       case "failed":
-        statusHtml = '<i class="fa fa-times-circle text-danger fa-2x" aria-hidden="true"></i>';
+        statusHtml = '<i class="fa fa-times-circle text-danger fa-2x" aria-hidden="true" title="Failed"></i>';
         break;
       case "applied":
-        statusHtml = '<i class="fa fa-check-circle-o text-success fa-2x" aria-hidden="true"></i>';
+        statusHtml = '<i class="fa fa-check-circle-o text-success fa-2x" aria-hidden="true" title="Running"></i>';
         break;
     }
 
@@ -94,6 +122,7 @@ MinionPoller = {
     } else {
       checked = '';
     }
+
     masterHtml = '<input name="roles[master][]" id="roles_master_' + minion.id +
       '" value="' + minion.id + '" type="radio" disabled="" ' + checked + '>';
 
