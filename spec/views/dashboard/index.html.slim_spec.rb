5bc7fd9ebb562a2c751ce72095f57816726a5b73
Flavio Castelli <fcastelli@suse.com>
2017-06-28 08:25:04 +0200

    AutoYaST: handle proxy settings [bsc#1043538]
    Handle the proxy settings inside of the AutoYaST file when the user
    enabled them at the node level.
    
    This is fixes bsc#1043538
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- spec/views/dashboard/index.html.slim_spec.rb
+++ spec/views/dashboard/index.html.slim_spec.rb
@@ -12,3 +12,31 @@ describe "dashboard/index" do
     end
   end
 end
+
+describe "dashboard/autoyast" do
+  context "when proxy is enabled" do
+    let(:http_proxy) { "squid.corp.net:3128"}
+    let(:https_proxy) { "squid.corp.net:3443"}
+    let(:no_proxy) { "localhost" }
+
+    before do
+      assign(:proxy_http, http_proxy)
+      assign(:proxy_https, https_proxy)
+      assign(:proxy_no_proxy, no_proxy)
+      assign(:proxy_systemwide, true)
+    end
+
+    it "generates a proxy section" do
+      render
+
+      section = assert_select "profile/networking/proxy/http_proxy"
+      expect(section.children.text).to eq(http_proxy)
+
+      section = assert_select "profile/networking/proxy/https_proxy"
+      expect(section.children.text).to eq(https_proxy)
+
+      section = assert_select "profile/networking/proxy/no_proxy"
+      expect(section.children.text).to eq(no_proxy)
+    end
+  end
+end
