07795f3486f6b5de4ad0d8ecfc5fe990abeca899
Rafael Fernández López <ereslibre@ereslibre.es>
2017-06-29 19:38:49 +0200

    Only restrict routes on production

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 # rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
   get "/autoyast", to: "dashboard#autoyast"
-  next if ENV["VELUM_PORT"].to_i != 443
+  next if Rails.env.production? && ENV["VELUM_PORT"].to_i != 443
   devise_for :users, controllers: { registrations: "auth/registrations",
                                     sessions:      "auth/sessions" }
 
