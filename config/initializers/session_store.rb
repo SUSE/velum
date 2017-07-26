2386314e2a1d3a99cc285366441311746303ab56
Maximilian Meister <mmeister@suse.de>
2017-07-26 10:11:57 +0200

    set secure flag in cookie (bsc#1050484)
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_velum_session"
+Rails.application.config.session_store :cookie_store,
+  key:    "_velum_session",
+  secure: !Rails.env.test?
