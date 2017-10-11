e7d877f6a27403bbc1357150f477891ca225f793
Maximilian Meister <mmeister@suse.de>
2017-10-11 10:04:23 +0200

    handle invalid sessions more user friendly
    if the session is not valid anymore, show this information to the
    user, and redirect him to the login instead of failing badly
    
    we dont need to raise in the stored_nonce method, as a failed
    deletion from the session hash will result in a nil value, meaning
    a non matching nonce
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- spec/lib/velum/kubernetes_spec.rb
+++ spec/lib/velum/kubernetes_spec.rb
@@ -15,4 +15,11 @@ describe Velum::Kubernetes do
     url = "#{endpoint.host}:#{endpoint.port}#{endpoint.path}"
     expect(url).to eq "example.test.lan:5900/api"
   end
+
+  it "returns a KubeConfig struct" do
+    allow(Velum::Salt).to receive(:call)
+      .and_return([nil, "return" => [file: "some file content"]])
+
+    expect(described_class.new.class.kubeconfig).to be_a(Velum::Kubernetes::KubeConfig)
+  end
 end
