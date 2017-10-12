d871bacd098019cf2522804453af8958d325274b
Maximilian Meister <mmeister@suse.de>
2017-10-12 11:15:05 +0200

    also compile svg
    the logo has recently changed to svg and needs to be compiled too
    otherwise it wont be shown
    
    bsc#1062878
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # include svg in the precompile
+  config.assets.precompile += %w( '.svg' )
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
