534605edbc83b4d40e4aeb0a6fe0540a41ca95e4
Rafael Fernández López <ereslibre@ereslibre.es>
2018-01-26 17:34:17 +0100

    Only require the relevant gem groups

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Velum
   class Application < Rails::Application
