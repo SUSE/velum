SUSE/velum

commit 6189bcad9e906816da1b827606889db2d4c259d0

Reduce salt-api timeout from 60 sec, to 30 sec
HAProxy has a 50 sec timeout, meaning HAProxy will kill the connection
before Velum times out waiting for salt-api when there are issues.

lib/velum/salt_api.rb
@@ -77,7 +77,14 @@ module Velum
           req.body = data.to_json if data.present?
         end
 
-        opts = { use_ssl: true, ca_file: "/etc/pki/ca.crt", ssl_version: :TLSv1, open_timeout: 2 }
+        opts = {
+          use_ssl:      true,
+          ca_file:      "/etc/pki/ca.crt",
+          ssl_version:  :TLSv1,
+          open_timeout: 2,
+          read_timeout: 30
+        }
+
         Net::HTTP.start(uri.hostname, uri.port, opts) { |http| http.request(req) }
       rescue *HTTPExceptions::EXCEPTIONS => e
         raise SaltConnectionException, e

commit ec69c13ccec9c6d45753be2a46dc251238669d07

Extend salt-api timeout as long as possible.
Allow enough time for the salt timeout, and a minion timeout before
cutting off the API call.

Should resolve
https://github.com/kubic-project/velum/issues/456
introduced in 6189bcad .

lib/velum/salt_api.rb
@@ -82,7 +82,7 @@ module Velum
           ca_file:      "/etc/pki/ca.crt",
           ssl_version:  :TLSv1,
           open_timeout: 2,
-          read_timeout: 30
+          read_timeout: 45
         }
 
         Net::HTTP.start(uri.hostname, uri.port, opts) { |http| http.request(req) }