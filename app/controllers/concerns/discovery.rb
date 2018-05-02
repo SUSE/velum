60cd084f58ebdcde953e329c53f9ac0a33ddd4c9
James Mason <jmason@suse.com>
2018-05-01 19:38:06 -0700

    Return cloud bootstrapping job progress in discovery
    BONUS: render discovery.html faster by not evaluating unused
    hash conditions.
    
    (cherry picked from commit f50a986edf8174cd032d01311cd968bce2d179e2)

--- app/controllers/concerns/discovery.rb
+++ app/controllers/concerns/discovery.rb
@@ -7,22 +7,18 @@ module Discovery
 
   # Responds with either an HTML or JSON version of the available minions.
   def discovery
-    assigned_minions                  = Minion.cluster_role
-    unassigned_minions                = Minion.unassigned_role
-    pending_minions                   = ::Velum::Salt.pending_minions
-    retryable_bootstrap_orchestration = Orchestration.retryable? kind: :bootstrap
-    retryable_upgrade_orchestration   = Orchestration.retryable? kind: :upgrade
-
     respond_to do |format|
       format.html
       format.json do
         hsh = {
-          assigned_minions:                  assigned_minions,
-          unassigned_minions:                unassigned_minions,
-          pending_minions:                   pending_minions,
+          assigned_minions:                  Minion.cluster_role,
+          unassigned_minions:                Minion.unassigned_role,
+          pending_minions:                   ::Velum::Salt.pending_minions,
+          pending_cloud_jobs:                SaltJob.all_open.count,
+          cloud_jobs_failed:                 SaltJob.failed.count,
           admin:                             Minion.find_by(minion_id: "admin"),
-          retryable_bootstrap_orchestration: retryable_bootstrap_orchestration,
-          retryable_upgrade_orchestration:   retryable_upgrade_orchestration
+          retryable_bootstrap_orchestration: Orchestration.retryable?(kind: :bootstrap),
+          retryable_upgrade_orchestration:   Orchestration.retryable?(kind: :upgrade)
         }
         render json: hsh
       end
