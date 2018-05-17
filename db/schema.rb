471f9eda08ad99f56d0d03cf002a308674fb27fa
Florian Bergmann <fbergmann@suse.de>
2018-05-17 10:23:48 +0200

    Move the migration to the end of the migration list by renaming.
    The tables for system_certificates were not part of the /var/lib/velum/schema.rb
    file and were not migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508070232) do
+ActiveRecord::Schema.define(version: 20181708070232) do
 
   create_table "certificate_services", force: :cascade do |t|
     t.integer  "certificate_id", limit: 4
