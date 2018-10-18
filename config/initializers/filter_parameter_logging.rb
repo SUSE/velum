302f59e2598db6238c413523162dc8235b051a6c
Chang Lee <clee@suse.com>
2018-10-18 16:33:31 -0700

    Velum: Add more log filters for user's secrets, pass, and tokens bug - 1111848 bsc#1111848

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :certificate, :bind_pw]
+Rails.application.config.filter_parameters += [:pass, :password, :bind_pw, :cert, :certificate, \
+                                               :client_secret, \
+                                               :authenticity_token, :id_token, :refresh_token]
